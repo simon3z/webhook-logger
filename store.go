44d225a0dbbeb49fd116e330d9009c455cbaea81
Julius Volz <julius.volz@gmail.com>
2017-06-15 12:51:26 -0400

    Add timestamping and garbage collection
    - Make the boltdb keys not only contain the received JSON blob, but
      wraps it in a whole "Notification" struct type as marshaled to JSON.
    - Add a Timestamp field to that struct.
    - GC old entries regularly.
    - Introduce options struct for configuring the boltStore.

--- store.go
+++ store.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"strconv"
+	"time"
 
 	"github.com/boltdb/bolt"
 	uuid "github.com/satori/go.uuid"
 )
 
+const (
+	bucketMetadata      = "metadata"
+	bucketNotifications = "notifications"
+
+	keyGenerationID = "generationID"
+)
+
 type notificationStore interface {
-	append([]byte) error
+	append(interface{}) error
 	get(generationID string, fromIndex uint64) (*NotificationsResponse, error)
 	close() error
 }
@@ -17,16 +27,29 @@ type notificationStore interface {
 type boltStore struct {
 	db           *bolt.DB
 	generationID string
+	options      *boltStoreOptions
+
+	stop chan struct{}
+	done chan struct{}
 }
 
-func newBoltStore(path string) (*boltStore, error) {
-	db, err := bolt.Open(path, 0600, nil)
+type boltStoreOptions struct {
+	retention  time.Duration
+	gcInterval time.Duration
+	path       string
+}
+
+func newBoltStore(opts *boltStoreOptions) (*boltStore, error) {
+	db, err := bolt.Open(opts.path, 0600, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	store := &boltStore{
-		db: db,
+		db:      db,
+		options: opts,
+		stop:    make(chan struct{}),
+		done:    make(chan struct{}),
 	}
 
 	err = db.Update(func(tx *bolt.Tx) error {
@@ -57,18 +80,46 @@ func newBoltStore(path string) (*boltStore, error) {
 	return store, nil
 }
 
+func (bs *boltStore) start() {
+	gcTicker := time.NewTicker(bs.options.gcInterval)
+	for {
+		select {
+		case <-bs.stop:
+			close(bs.done)
+		case <-gcTicker.C:
+			log.Println("Running GC cycle to remove old entries...")
+			num, err := bs.gc(time.Now().Add(-bs.options.retention))
+			if err != nil {
+				log.Println("Error running GC cycle:", err)
+			} else {
+				log.Printf("Deleted %d old entries", num)
+			}
+		}
+	}
+}
+
 func keyFromIndex(index uint64) []byte {
 	return []byte(strconv.FormatUint(index, 10))
 }
 
-func (bs *boltStore) append(notification []byte) error {
+func (bs *boltStore) append(data interface{}) error {
 	return bs.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucketNotifications))
 		idx, err := b.NextSequence()
 		if err != nil {
 			return fmt.Errorf("error getting next sequence number: %v", err)
 		}
-		if err := b.Put(keyFromIndex(idx), notification); err != nil {
+
+		n := Notification{
+			Index:     idx,
+			Timestamp: time.Now(),
+			Data:      data,
+		}
+		buf, err := json.Marshal(n)
+		if err != nil {
+			return fmt.Errorf("error marshalling notification: %v", err)
+		}
+		if err := b.Put(keyFromIndex(idx), buf); err != nil {
 			return fmt.Errorf("error appending notification: %v", err)
 		}
 		return nil
@@ -87,16 +138,13 @@ func (bs *boltStore) get(generationID string, fromIndex uint64) (*NotificationsR
 			k, v = c.First()
 		}
 
+		var n Notification
 		for ; k != nil; k, v = c.Next() {
-			idx, err := strconv.ParseUint(string(k), 10, 64)
-			if err != nil {
-				return fmt.Errorf("error parsing index from key: %v", err)
+			if err := json.Unmarshal(v, &n); err != nil {
+				return fmt.Errorf("unable to unmarshal notification: %v", err)
 			}
 
-			ns = append(ns, Notification{
-				Index: idx,
-				Data:  JSONString(v),
-			})
+			ns = append(ns, n)
 		}
 		return nil
 	})
@@ -110,6 +158,35 @@ func (bs *boltStore) get(generationID string, fromIndex uint64) (*NotificationsR
 	}, nil
 }
 
+func (bs *boltStore) gc(olderThan time.Time) (int, error) {
+	var numDeleted int
+	return numDeleted, bs.db.Update(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(bucketNotifications)).Cursor()
+
+		// For now, this goes through all entries and doesn't abort after the first
+		// encountered entry that should be kept, just in case there are time/date
+		// glitches on a machine and timestamps end up being out of order.
+		//
+		// TODO: Possibly reconsider this for performance reasons if the DB gets huge.
+		var n Notification
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			if err := json.Unmarshal(v, &n); err != nil {
+				return fmt.Errorf("unable to unmarshal notification: %v", err)
+			}
+
+			if n.Timestamp.Before(olderThan) {
+				if err := c.Delete(); err != nil {
+					return fmt.Errorf("unable to delete notification: %v", err)
+				}
+				numDeleted++
+			}
+		}
+		return nil
+	})
+}
+
 func (bs *boltStore) close() error {
+	close(bs.stop)
+	<-bs.done
 	return bs.db.Close()
 }
